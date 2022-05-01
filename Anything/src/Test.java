1b117eb66f11a09deb6ec87a581314f04ec50fab
JKHM1112 <jk007125@naver.com>
2022-05-01 22:59:16 +0900

    5월1일

--- Anything/src/Test.java
+++ Anything/src/Test.java
@@ -1,11 +1,8 @@
-class A {
-static int abc=100;
+public class Test {												
+	public static void main(String[] args) {											
+		
+		java.util.Scanner s =new java.util.Scanner(System.in);
+		int input = s.nextInt();
+	}
+	
 }
-class Test{
-	public static void main(String[] args) {
-		System.out.println("A.abc = " + A.abc);
-		A a1 = new A();
-		a1.abc=200;
-		System.out.println("a1.abc = " + a1.abc);
-}
-} 
