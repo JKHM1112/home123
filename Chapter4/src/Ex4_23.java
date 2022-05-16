e6ebc1865c0b550df777a97a38c9569121f8ba3b
JKHM <jk007125@naver.com>
2022-05-16 11:42:19 +0900

    5월16일학원아침

--- Chapter4/src/Ex4_23.java
+++
@@ -1,9 +0,0 @@
-public class Ex4_23 {
-	public static void main(String[] args) {
-		int i=5;
-		
-		while(i--!=0) {     //조건식이 평가된후 계산이 된다.
-			System.out.println(i + " - I can do it.");
-		}
-	}
-}
